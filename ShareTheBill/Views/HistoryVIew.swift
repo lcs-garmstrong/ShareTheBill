be0a4cdcaff7a5378ab4039823550e4f53e974cf
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-02-02 15:08:55 -0500

    added the history view

---
+++ ShareTheBill/Views/HistoryVIew.swift
@@ -0,0 +1,35 @@
+//
+//  HistoryVIew.swift
+//  ShareTheBill
+//
+//  Created by Graeme Armstrong on 2023-02-02.
+//
+
+import SwiftUI
+
+struct HistoryVIew: View {
+    //MARK: Stored properties
+    
+    @Binding var history: [Result]
+
+    // MARK: Coputed properties
+    var body: some View {
+        VStack {
+            
+            Text("History")
+                .font(.headline.smallCaps())
+                .padding()
+            
+            List(history.reversed()) { somePriorResult in
+                ResultView(priorResult: somePriorResult)
+            }
+
+        }
+    }
+}
+
+//struct HistoryVIew_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HistoryVIew()
+//    }
+//}
